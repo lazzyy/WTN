ce738c769146876a36683f493442e3061fef0b6a
mylaptop <mylaptop@RAM>
2020-08-01 14:08:06 +0530

    The exercises in Collections

---
+++ Milestone2/src/com/wipro/collections/list/Assignment1_ArrayList_months.java
@@ -0,0 +1,27 @@
+package com.wipro.collections.list;
+
+import java.util.ArrayList;
+
+public class Assignment1_ArrayList_months 
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayList<String> month= new ArrayList<String>();
+	       month.add("January");
+	       month.add("Febuary");
+	       month.add("March");
+	       month.add("April");
+	       month.add("May");
+	       month.add("June");
+	       month.add("July");
+	       month.add("August");
+	       month.add("September");
+	       month.add("October");
+	       month.add("November");
+	       month.add("December");
+	       for(String s:month)
+	    	   System.out.println(s);
+	}
+
+}
